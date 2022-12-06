62fdc5c4865212b884796ef0e2a9a1ba7181ddba
Molnár Tamás <molnarj@EOTVOS2.LOCAL>
2022-12-06 14:24:10 +0100

    masodik

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,24 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
-    return view('welcome');
+    $kategoriak = DB::select("SELECT kategoriak.nev, urlek.url FROM 
+    kategoriak INNER JOIN urlek ON (kategoriak.k_id=urlek.kapcsolat) WHERE urlek.tipus='kategoria'"); 
+    return view('welcome',["kategoriak" => $kategoriak]);
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
